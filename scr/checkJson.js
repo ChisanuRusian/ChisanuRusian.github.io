3f684bba714cc083e83149789bc742eaa4352e14
ChisanuRusian <binh.email.unity.money@gmail.com>
2024-03-22 17:56:35 +0700

    Update checkJson.js

--- scr/checkJson.js
+++ scr/checkJson.js
@@ -1,4 +1,4 @@
-async function openFolder(){
+async function openFolder(extensionAllow = ['.json']) {
   const input = document.createElement('input');
   input.type = 'file';
   input.webkitdirectory = true;
@@ -10,8 +10,7 @@ async function openFolder(){
     input.addEventListener('change', async () => {
       const paths = [];
       for (const file of input.files) {
-        if (file.name.endsWith('.json')) {
-          console.log(`Adding ${file.webkitRelativePath}`);
+        if (extensionAllow.some(ext => file.name.endsWith(ext))) {
           paths.push({ path: file.webkitRelativePath, file: URL.createObjectURL(file) });
         }
       }
@@ -20,41 +19,70 @@ async function openFolder(){
   })
 }
 
-async function minifyAllJsonAndZip(paths) {
-  const minifiedFiles = await Promise.all(paths.map(async (path) => {
-    const response = await fetch(path.file);
-    const json = await response.json();
+
+async function minifyAllJson(paths) {
+  const minifiedFiles = await Promise.all(paths.map(async ({ path, file }) => {
+    const json = await (await fetch(file)).json();
+    const [, ...folderParts] = path.split('/');
+    const root = folderParts.pop();
+    const fileName = path.split('/').pop();
     const minifiedJson = JSON.stringify(json, null, 2);
-    const fileNameParts = path.path.split('/');
-    const folderParts = fileNameParts.slice(0, -1);
-    const fileName = fileNameParts.pop();
-    const minifiedFileName = fileName.split('.').shift() + '.json';
     const minifiedFile = new File(
       [minifiedJson],
-      folderParts.concat(minifiedFileName).join('/'),
+      [...folderParts, fileName].join('/'),
       { type: 'application/json' }
     );
     return minifiedFile;
   }));
-  
+
+  return minifiedFiles;
+}
+
+async function minifyAllXml(paths) {
+  const minifiedFiles = await Promise.all(paths.map(async ({ path, file }) => {
+    const xmlText = await (await fetch(file)).text();
+    const [, ...folderParts] = path.split('/');
+    const root = folderParts.pop();
+
+    const fileName = path.split('/').pop();
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xmlText, 'application/xml');
+    const xmlString = new XMLSerializer().serializeToString(xmlDoc);
+    const minifiedXml = vkbeautify.xmlmin(xmlString);
+    const minifiedFile = new File(
+      [minifiedXml],
+      [...folderParts, fileName].join('/'),
+      { type: 'application/xml' }
+    );
+    return minifiedFile;
+  }));
+
+  return minifiedFiles;
+}
+
+async function doZip(root, minifiedFiles) {
   const zip = new JSZip();
   for (const file of minifiedFiles) {
     zip.file(file.name, file);
   }
 
-  const zipBlob = await zip.generateAsync({type: 'blob'});
+  const zipBlob = await zip.generateAsync({ type: 'blob' });
   const zipUrl = URL.createObjectURL(zipBlob);
-  const downloadLink = document.createElement('a');
-  downloadLink.href = zipUrl;
-  downloadLink.download = `minified_json.zip`;
-  downloadLink.click();
+  const link = document.createElement('a');
+  link.href = zipUrl;
+  link.download = root + '.zip';
+  link.click();
   URL.revokeObjectURL(zipUrl);
 }
 
-
-async function doActions() {
+async function doMinifyJson() {
   const paths = await openFolder();
-  await minifyAllJsonAndZip(paths);
+  const minifiedFiles = await minifyAllJson(paths);
+  await doZip("json_compress", minifiedFiles);
 }
 
-
+async function doMinifyXml() {
+  const paths = await openFolder(['.plist', '.xml']);
+  const minifiedFiles = await minifyAllXml(paths);
+  await doZip("xml_compress", minifiedFiles);
+}
